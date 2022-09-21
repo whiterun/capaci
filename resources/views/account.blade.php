6976c9ea8feeb3bdb2714fe635954227a142565a
whiterun <winterhold89@gmail.com>
2022-09-21 23:57:51 +0700

    set basic detail view

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -9,6 +9,33 @@
   </div>
 
   <div class="my-3 p-3 bg-body rounded shadow-sm">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col-lg-4">
+          <h6 class="border-bottom pb-2 mb-3 text-center">Basic Detail</h6>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Name</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name" value="{{Auth::user()->name}}">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" value="{{Auth::user()->email}}">
+          </div>
+          <div class="d-grid mb-3">
+            <button class="btn btn-primary" type="submit">Update</button>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <h6 class="border-bottom pb-2 mb-3 text-center">Password</h6>
+        </div>
+        <div class="col-lg-4">
+          <h6 class="border-bottom pb-2 mb-3 text-center">Subscription</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="my-3 p-3 bg-body rounded shadow-sm">
     <h6 class="border-bottom pb-2 mb-0">Video collections</h6>
     <div class="d-flex text-muted pt-3">
       <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
